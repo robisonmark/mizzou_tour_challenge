56dabb888f51a3b579df963251b43b180f1c4e27
Mark Robison <markrobison@iprosystems.com>
2020-04-16 22:39:27 -0500

    added in eslint packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,79 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const notify = require('gulp-notify');
-const autoprefix = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const notify = require('gulp-notify')
+const rename = require('gulp-rename')
+const autoprefix = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const babel = require('gulp-babel')
+const eslint = require('gulp-eslint')
+const connect = require('gulp-connect')
 
 // File Dir Variables
+var htmlDir = './src/**/*.html'
+var targetHtml = './dist/'
+
+// images
+var imageDir = './src/images/*'
+var targetImage = './dist/images/'
+
+// Styles
+var sassSource = './src/scss/main.scss'
 var sassDir = './src/scss/**/*.scss'
-var targetCSS = './dist/css'
+var targetCSS = './dist/css/'
+
+// javascript
+var jsSource = './src/js/main.js'
 var jsDir = './src/js'
 var targetJs = './dist/js'
 
-
-function css() {
-    return src(sassDir)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefix())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(targetCSS))
-        .pipe(notify('CSS Updated'))
+function server () {
+  return connect.server({
+    root: 'dist',
+    port: 8888,
+    livereload: true
+  })
 }
 
-function js() {
-    return src(jsDir)
-        .pipe(sourcemaps.init())
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(targetJs))
+function html () {
+  return gulp.src(htmlDir)
+    .pipe(gulp.dest(targetHtml))
+    .pipe(connect.reload())
+}
+
+function images () {
+  return gulp.src(imageDir)
+    .pipe(gulp.dest(targetImage))
+    .pipe(connect.reload())
+}
+
+function css () {
+  return gulp.src(sassSource)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefix())
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest(targetCSS, { overwrite: true, sourcemaps: true }))
+    .pipe(notify('CSS Updated'))
+    .pipe(connect.reload())
+}
+
+function js () {
+  return gulp.src(jsSource)
+    .pipe(sourcemaps.init())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(targetJs))
+    .pipe(connect.reload())
 }
 
 exports.default = () => {
-    gulp.watch(sassDir, css);
-    gulp.watch(jsDir, js)
+  server()
+  gulp.watch(htmlDir, html)
+  gulp.watch(imageDir, images)
+  gulp.watch(sassDir, css)
+  gulp.watch(jsDir, js)
 }
