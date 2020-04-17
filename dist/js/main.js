f8b829562925980dea887be8436d365ca0776ec5
Mark Robison <markrobison@iprosystems.com>
2020-04-16 22:44:54 -0500

    complied files while working through styles

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _index = _interopRequireDefault(require("./router/index"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _index["default"])();
+//# sourceMappingURL=main.js.map
