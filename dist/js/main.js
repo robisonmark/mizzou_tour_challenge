9b0b9123d93fdbe017d6aa03ddfb04316270c116
Mark Robison <markrobison@iprosystems.com>
2020-04-19 16:32:55 -0500

    running build

--- dist/js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("D:\\markrobison\\sites\\mizzou_tour_challenge\\node_modules\\@babel\\runtime/helpers/interopRequireDefault");
-
-var _index = _interopRequireDefault(require("./router/index"));
-
-(0, _index["default"])();
-//# sourceMappingURL=main.js.map
